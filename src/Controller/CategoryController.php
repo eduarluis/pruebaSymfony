b986bc049b22750f5668e3198e94f0c078d115bb
Eduardo <eduarluis96@gmail.com>
2021-02-07 22:50:36 -0400

    ajustes en los controladores products y category Crud completo

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Categories;
+use App\Form\CategoryType;
+
 class CategoryController extends AbstractController
 {
     /**
@@ -13,53 +18,84 @@ class CategoryController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
-        ]);
+        $em = $this->getDoctrine()->getManager();
+
+        $category = $em->getRepository(Categories::class)->findAll();
+
+        return $this->render('category/index.html.twig', ['categories'=>$category]);
     }
 
     /**
-     * @Route("/products/create", name="cCreate")
+     * @Route("category/create", name="cCreate")
      */
-    public function create()
+    public function create(Request $request)
     {
-        return $this->render('category/create.html.twig');
+        $categories = new Categories();
+
+        $form = $this->createForm(CategoryType::class, $categories);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $categories->setCreatedAt( new \DateTime());
+            $categories->setUpdatedAt( new \DateTime());
+
+            $categories = $form->getData();
+
+            $entityManager->persist($categories);
+            $entityManager->flush(); 
+            
+            return $this->redirectToRoute('category');
+            
+        }
+
+        return $this->render('category/create.html.twig',['form' => $form->createView()]);
     }
 
     /**
-     * @Route("/products/create", name="cStore")
-     */
-    public function store($requet)
-    {
-
-    }
-
-    /**
-     * @Route("/products/{id}", name="cShow")
+     * @Route("category/{id}", name="cShow")
      */
     public function show(int $id)
     {
-        return $this->render('category/show.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $category = $em->getRepository(Categories::class)->find($id); 
+        return $this->render('category/show.html.twig',['category' => $category]);
     }
 
     /**
-     * @Route("/products/{id}/edit", name="cEdit")
+     * @Route("category/{id}/edit", name="cEdit")
      */
-    public function edit(int $id)
+    public function edit(int $id,Request $request)
     {
-        return $this->render('category/edit.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $category = $em->getRepository(Categories::class)->find($id);
+
+        if (!$category) {
+          throw $this->createNotFoundException(
+                  'No category found for id ' . $id
+          );
+        }
+        
+        $form = $this->createForm(CategoryType::class, $category);
+    
+        $form->handleRequest($request);
+     
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            // return new Response('category updated successfully');
+            return $this->redirectToRoute('category');
+        }
+        
+        $build['form'] = $form->createView();
+
+        return $this->render('category/edit.html.twig',$build);
     }
 
     /**
-     * @Route("/products/{id}", name="cUpdate")
-     */
-    public function update($request, int $id)
-    {
-
-    }
-
-    /**
-     * @Route("/products/{id}", name="cDestroy")
+     * @Route("category/{id}", name="cDestroy")
      */
     public function destroy(int $id)
     {
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Products;
+use App\Form\ProductsType;
+
 class ProductController extends AbstractController
 {
     /**
@@ -13,25 +18,44 @@ class ProductController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository(Products::class)->findAll();
+
+        return $this->render('product/index.html.twig',['products' => $products]);
     }
 
     /**
      * @Route("/products/create", name="pCreate")
      */
-    public function create()
+    public function create(Request $request)
     {
-        return $this->render('product/create.html.twig');
-    }
+        $product = new Products();
 
-    /**
-     * @Route("/products/create", name="pStore")
-     */
-    public function store($requet)
-    {
+        $form = $this->createForm(ProductsType::class, $product);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            
+            $data = $form->getData();
+            $product->setCreatedAt( new \DateTime());
+            $product->setUpdatedAt( new \DateTime());
+
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Se registro con exito el producto!'
+            );
+
+            return $this->redirectToRoute('pCreate');
+        }
+
+        return $this->render('product/create.html.twig',['form' => $form->createView()]);
     }
 
     /**
@@ -39,30 +63,61 @@ class ProductController extends AbstractController
      */
     public function show(int $id)
     {
-        return $this->render('product/show.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository(Products::class)->find($id);
+
+        return $this->render('product/show.html.twig',['product' => $product]);
     }
 
     /**
      * @Route("/products/{id}/edit", name="pEdit")
      */
-    public function edit(int $id)
+    public function edit(int $id,Request $request)
     {
-        return $this->render('product/edit.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository(Products::class)->find($id);
+        if (!$product) {
+          throw $this->createNotFoundException(
+                  'No product found for id ' . $id
+          );
+        }
+        
+        $form = $this->createForm(ProductsType::class, $product);
+    
+        $form->handleRequest($request);
+     
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            // return new Response('Product updated successfully');
+            $this->addFlash(
+                'notice',
+                'Se actualizo con exito el producto!'
+            );
+            return $this->redirectToRoute('products');
+        }
+        
+        $build['form'] = $form->createView();
+
+        return $this->render('product/edit.html.twig', $build);
     }
 
     /**
-     * @Route("/products/{id}", name="pUpdate")
-     */
-    public function update($request, int $id)
-    {
-
-    }
-
-    /**
-     * @Route("/products/{id}", name="pDestroy")
+     * @Route("/products/{id}/delete", name="pDestroy")
      */
     public function destroy(int $id)
     {
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository(Products::class)->find($id);
+
+        $em->remove($product);
+        $em->flush($product);
+
+        $this->addFlash(
+            'notice',
+            'Se elimino con exito el producto!'
+        );
+
+        return $this->redirectToRoute('products');
 
     }
 }
